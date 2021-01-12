5536c9b286cb3d7af84fc6dd13c6d30e9c11641a
doomsdayrs <doomsdayrs@gmail.com>
2021-01-12 16:01:01 -0500

    - created Extensions.kt with new sendEmbed extension function

---
+++ src/main/kotlin/com/github/doomsdayrs/lib/javacordktx/embedBuilder/Extensions.kt
@@ -0,0 +1,19 @@
+package com.github.doomsdayrs.lib.javacordktx.embedBuilder
+
+import kotlinx.coroutines.future.await
+import org.javacord.api.entity.message.Message
+import org.javacord.api.entity.message.Messageable
+import org.javacord.api.entity.message.embed.EmbedBuilder
+
+
+/**
+ * Convenience function that lets you build an embed without specifying one as a parameter
+ * This is suspended to conform to proper kotlin async using [await]
+ *
+ * @author Doomsdayrs
+ * @see await
+ * @param builder Builder for the embed
+ * @return Message after [await] completes
+ */
+suspend fun Messageable.sendEmbed(builder: EmbedBuilder.() -> Unit): Message =
+	sendMessage(embedBuilder(builder)).await()
